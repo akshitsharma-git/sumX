366e4dbdc60ec7301e5c2760c795023c15c0f63f
akshitsharma-git <work.akshitsharma@gmail.com>
2025-12-02 15:24:29 +0530

    updated vercel.json 3

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,33 @@
-    const mongoose=require("mongoose")
-    const cors=require("cors")
-    const dotenv=require("dotenv")
-    const jwt=require("jsonwebtoken")
-    const express=require("express")
-    const cookieParser=require("cookie-parser")
+const mongoose=require("mongoose")
+const cors=require("cors")
+const dotenv=require("dotenv")
+const jwt=require("jsonwebtoken")
+const express=require("express")
+const cookieParser=require("cookie-parser")
 
-    const home=require("./routes/home.route.js")
+const home=require("./routes/home.route.js")
 
-    dotenv.config()
+dotenv.config()
 
-    const app=express()
-    const PORT=process.env.PORT || 1111
+const app=express()
+const PORT=process.env.PORT || 1111
 
-    mongoose.connect(process.env.MONGO_URL).then(e=>console.log("MongoDB Connected")).catch((err)=>console.error("MongoDB Connection Error: ",err))
+mongoose.connect(process.env.MONGO_URL).then(e=>console.log("MongoDB Connected")).catch((err)=>console.error("MongoDB Connection Error: ",err))
 
-    app.use(cors({
-        origin: "http://localhost:5173",
-        credentials: true
-    }))
-    app.use(express.json())
-    app.use(express.urlencoded({extended:true}))
-    app.use(cookieParser())
+app.use(cors({
+    origin: process.env.NODE_ENV==='production'?true:"http://localhost:5173",
+    credentials: true
+}))
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser())
 
-    app.use("/",home)
+app.use("/",home)
 
+if(process.env.NODE_ENV !== 'production') {
     app.listen(PORT,()=>{
         console.log(`Server is running at http://localhost:${PORT}`)
     })
+}
+
+module.exports=app
