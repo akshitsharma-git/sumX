5abe282da78294d89630daee517f164926b86542
akshitsharma-git <work.akshitsharma@gmail.com>
2025-12-02 16:00:50 +0530

    updated vercel.json 6

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,40 @@
-const mongoose=require("mongoose")
-const cors=require("cors")
-const dotenv=require("dotenv")
-const jwt=require("jsonwebtoken")
-const express=require("express")
-const cookieParser=require("cookie-parser")
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const jwt = require("jsonwebtoken");
+const express = require("express");
+const cookieParser = require("cookie-parser");
 
-const home=require("./routes/home.route.js")
+const home = require("./routes/home.route.js");
 
-dotenv.config()
+dotenv.config();
 
-const app=express()
-const PORT=process.env.PORT || 1111
+const app = express();
 
-mongoose.connect(process.env.MONGO_URL).then(e=>console.log("MongoDB Connected")).catch((err)=>console.error("MongoDB Connection Error: ",err))
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.error("MongoDB Connection Error:", err));
 
-app.use(cors({
-    origin: process.env.NODE_ENV==='production'?true:"http://localhost:5173",
-    credentials: true
-}))
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use(cookieParser())
+app.use(
+  cors({
+    origin:
+      process.env.NODE_ENV === "production" ? "*" : "http://localhost:5173",
+    credentials: true,
+  })
+);
 
-app.use("/",home)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-if(process.env.NODE_ENV !== 'production') {
-    app.listen(PORT,()=>{
-        console.log(`Server is running at http://localhost:${PORT}`)
-    })
+app.use("/", home);
+
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 1111;
+  app.listen(PORT, () =>
+    console.log(`Server running locally at http://localhost:${PORT}`)
+  );
 }
 
-module.exports=app
+module.exports = app;
